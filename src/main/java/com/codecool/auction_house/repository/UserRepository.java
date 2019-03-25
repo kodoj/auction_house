2352d2ed3c7778f8cd2d1cbd907b0592b2166918
Krzysztof Odoj <kodoj1@o2.pl>
2019-03-25 15:07:21 +0100

    JPASpecificationExecutor added to extends

--- src/main/java/com/codecool/auction_house/repository/UserRepository.java
+++ src/main/java/com/codecool/auction_house/repository/UserRepository.java
@@ -2,9 +2,10 @@ package com.codecool.auction_house.repository;
 
 import com.codecool.auction_house.model.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
 }
