de2b257a0822f65984fb35ab53550ec8fe8c3a6e
Krzysztof Odoj <kodoj1@o2.pl>
2019-03-25 13:10:29 +0100

    hashing password fixed MD5 method used

--- src/main/java/com/codecool/auction_house/controller/PasswordHasher.java
+++ src/main/java/com/codecool/auction_house/controller/PasswordHasher.java
@@ -1,36 +1,32 @@
 package com.codecool.auction_house.controller;
 
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 
 public class PasswordHasher {
 
-    public static String hashPassword(String password) {
-        String hashedPassword = null;
-        try {
-            byte[] salt = getSalt();
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            md.update(salt);
-            byte[] passwordBytes = md.digest(password.getBytes());
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i< passwordBytes.length ;i++)
-            {
-                sb.append(Integer.toString((passwordBytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-            hashedPassword = sb.toString();
-        }
-        catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return hashedPassword;
-    }
-
-    private static byte[] getSalt() throws NoSuchAlgorithmException
+    public static String hashPassword(String password)
     {
-        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-        byte[] salt = new byte[16];
-        sr.nextBytes(salt);
-        return salt;
+        try {
+
+            // Static getInstance method is called with hashing MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            byte[] messageDigest = md.digest(password.getBytes());
+
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
