fe84aeb8e6f242408fc4ea77d4b3520eb6972f01
Krzysztof Odoj <kodoj1@o2.pl>
2019-04-03 11:19:29 +0200

    log4j2 configured, added delete button to account view

---
+++ src/test/java/com/codecool/auction_house/logger/Log4J2PropertiesConfTest.java
@@ -0,0 +1,14 @@
+package com.codecool.auction_house.logger;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Log4J2PropertiesConfTest {
+
+    @Test
+    public void testPerformSomeTask() throws Exception {
+        Log4J2PropertiesConf log4J2PropertiesConf=new Log4J2PropertiesConf();
+        log4J2PropertiesConf.performSomeTask();
+    }
+}
